6b46eb5a6196cd37aa4b8f1169c10231874b2d82
jorian <jorian@outlook.com>
2020-01-08 13:44:38 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+extern crate komodo_airdrop;
+extern crate komodo_rpc_client;
+extern crate csv;
+extern crate serde;
+
+use komodo_airdrop::Snapshot;
+use komodo_rpc_client::Chain;
+use std::collections::HashMap;
+use csv::Error;
+
+fn main() {
+    let snapshot = Snapshot::builder()
+        .on_chain(Chain::Custom("ILN".to_string()))
+        .using_threshold(1.0)
+        .build()
+        .expect("Failed to create snapshot of ILN chain");
+
+    let mut hashm = HashMap::new();
+    snapshot.addresses.iter().for_each(|address| {
+        hashm.insert(address.addr.clone(), address.amount);
+    });
+
+    write_hashmap_to_csv(&hashm).expect("Problem during CSV creation");
+}
+
+fn write_hashmap_to_csv(map: &HashMap<String, f64>) -> Result<(), Error> {
+    let mut writer = csv::Writer::from_path("./snapshot.csv").expect("Problem while creating snapshot file");
+
+    map.iter().for_each(|(key, value)| {
+        writer.serialize((key, value)).expect("Problem during serialization");
+    });
+
+    Ok(())
+}
